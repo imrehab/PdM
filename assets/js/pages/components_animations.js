55d377a7f5c22a0ca05a1378d9d3c4b132410ba4
Nujood Ahmed <nujjod@yahoo.com>
2019-11-15 18:06:50 +0300

    delete files

--- assets/js/pages/components_animations.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('#replay-animations').on('click',function(e) {
-        e.preventDefault();
-        altair_helpers.hierarchical_show('#hierarchical-show');
-        altair_helpers.hierarchical_slide('#hierarchical-slide');
-    })
-});
--- assets/js/pages/ecommerce_payment_page.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('[name="pm"]').on('ifChecked',function() {
-        var $this = $(this);
-        $('.js-pm_info').not($this).slideUp('fast');
-        $('.' + $this.attr('id')).slideDown('fast');
-    })
-});
