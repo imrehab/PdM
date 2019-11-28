8d1657853b5e04e63f45aef3d6f14e4b3858a429
Nujood Ahmed <nujjod@yahoo.com>
2019-11-28 20:58:27 +0300

    Add files

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ from sklearn.metrics import r2_score, make_scorer, mean_squared_error, mean_abso
 from sklearn.feature_selection import SelectFromModel
 import pyfireconnect
 from whitenoise import WhiteNoise
+import pandas as pd
+import random
+from sklearn.utils import shuffle
+from sklearn.model_selection import ShuffleSplit, learning_curve, train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 config = {
   "apiKey": "AIzaSyAal-QcuayT4d32G-6YJLw8m7Um5b1BPrg",
@@ -22,24 +27,23 @@ config = {
   "databaseURL": "https://raspberrypi-6b521.firebaseio.com",
   "storageBucket": "raspberrypi-6b521.appspot.com"
 }
-
+#
 firebase = pyfireconnect.initialize(config)
 datab = firebase.database()
-data = datab.child("sensor/data").get()
-# .order_by_child("name").limit_to_first(1).get()
+data = datab.child("sensor/MPU0001").get()
 for data in data.each():
     key = data.key()
-#     print(user.val())
     break
 
 data_split = []
-data_key = datab.child("sensor/data/"+key).get()
-# print(users2.val())
+data_key = datab.child("sensor/MPU0001/"+key).get()
 for data in data_key.each():
-#     print(user.key())
-#     print(user.val())
     data_split.append(data.val())
 
+# data_split[0] -> acc -> data_split[0][0] , data_split[0][1], data_split[0][2]
+# data_split[1] -> asset id
+# data_split[2] -> Temp
+# data_split[3] -> time
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app)
@@ -50,11 +54,30 @@ my_static_folders = (
 for static in my_static_folders:
     app.wsgi_app.add_files(static)
 
+data = pd.read_csv("data.csv")
+data = data.drop(columns=['||__time'])
+data = shuffle(data)
+feature_cols = data.drop(['Normality'], axis = 1).columns
+X = data[feature_cols]
+y = data.Normality
+
+# split the data into test/train
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+neigh = KNeighborsClassifier(n_neighbors=7)
+neigh.fit(X_train, y_train)
+pred = neigh.predict([[data_split[0][0],data_split[0][1], data_split[0][2], data_split[2]]])
+prob = neigh.predict_proba([[data_split[0][0],data_split[0][1], data_split[0][2], data_split[2]]])
+str = str(prob).strip('[]').split()
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
 
-    return render_template('index.html', temp=data_split[0])
+
+    return render_template('index.html', prob=float(str[1])*100, normality=pred)
+
+
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
@@ -84,7 +107,7 @@ def timeline():
 
 @app.route('/asset.html')
 def asset():
-    return render_template('asset.html')
+    return render_template('asset.html',  prob=float(str[1])*100)
 
 
 @app.route('/assets.html')
